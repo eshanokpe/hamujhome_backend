fd6fa86cf5917ae9c227499734998c958827bca8
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-25 06:38:56 +0100

    Updated Changes

--- resources/views/homePage/master.blade copy.php
+++ resources/views/homePage/master.blade copy.php
@@ -1,43 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en-US">
 <head>
-	<meta charset="UTF-8">
-	<meta name="keywords" content="Real estate, Property sale, Property buy">
-	<meta name="description" content="Homy is a beautiful website template designed for Real Estate Agency.">
-    <meta property="og:site_name" content="Homy">
-    <meta property="og:url" content="https://creativegigstf.com">
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="Homy-Real Estate HTML5 Template & Dashboard">
-	<meta name='og:image' content='images/assets/ogg.png'>
-	<!-- For IE -->
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<!-- For Resposive Device -->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- For Window Tab Color -->
-	<!-- Chrome, Firefox OS and Opera -->
-	<meta name="theme-color" content="#0D1A1C">
-	<!-- Windows Phone -->
-	<meta name="msapplication-navbutton-color" content="#0D1A1C">
-	<!-- iOS Safari -->
-	<meta name="apple-mobile-web-app-status-bar-style" content="#0D1A1C">
-	<title>Homy-Real Estate HTML5 Template & Dashboard</title>
-	<!-- Favicon -->
-	<link rel="icon" type="image/png" sizes="56x56" href="images/fav-icon/icon.png">
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" media="all">
-	<!-- Main style sheet -->
-	<link rel="stylesheet" type="text/css" href="css/style.css" media="all">
-	<!-- responsive style sheet -->
-	<link rel="stylesheet" type="text/css" href="css/responsive.css" media="all">
-
-	<!-- Fix Internet Explorer ______________________________________-->
-	<!--[if lt IE 9]>
-			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-			<script src="vendor/html5shiv.js"></script>
-			<script src="vendor/respond.js"></script>
-		<![endif]-->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="author" content="{{$website->author}}"/>
+    <meta name="keywords" content="{{$website->keywords}}" />
+    <meta name="url" content="{{$website->url}}" />
+    <meta name="google-site-verification" content="{{$website->google}}">
+    <meta name="">
+    <title>@yield('title')</title>
+    <meta name="tags" content="{{$website->tags}}" />
+    <meta name="description" content="{{$website->description}}" />
+    @yield('meta')
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset($website->fav_icon)}}">
+    <link rel="stylesheet" href="{{asset('static')}}/assets/css/style.css">
+    <link rel="stylesheet" href="{{asset('static')}}/assets/css/widgets.css">
+    <link rel="stylesheet" href="{{asset('static')}}/assets/css/responsive.css">
+    <link href="{{asset('frontEnd')}}/iziToast/dist/css/iziToast.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-
+ 
 <body>
     <div class="scroll-progress primary-bg"></div>
     @include('homePage.inc.header')
