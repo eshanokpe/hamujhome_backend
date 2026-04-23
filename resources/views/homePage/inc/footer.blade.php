a784f10f08ca94a85eb1d5697f04e97aae1f73ca
Infoscert <infoscert@Infoscerts-MBP.lan>
2026-04-23 11:52:10 +0100

    chore: update footer

--- resources/views/homePage/inc/footer.blade.php
+++ resources/views/homePage/inc/footer.blade.php
@@ -218,7 +218,7 @@
 
 <!-- Optional JavaScript _____________________________  -->
 <!--Start of Tawk.to Script-->
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -228,7 +228,7 @@ s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-</script>
+</script> -->
 <!--End of Tawk.to Script-->
 
 <!-- jQuery first, then Bootstrap JS -->
